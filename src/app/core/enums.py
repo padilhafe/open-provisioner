2c314ebd639df1ab0628fd1d0204c59443c17321
Felipe Duellis Padilha <felipe.padilha@brasilnetcp.net.br>
2025-04-10 08:48:17 -0300

    feat: added new enums

--- src/app/core/enums.py
+++ src/app/core/enums.py
@@ -2,12 +2,34 @@
 
 from enum import Enum
 
+# Users Enums
+class UserRoles(str, Enum):
+    OPERATOR = "operator"
+    TECHINICIAN = "techinician"
+    ADMIN = "admin"
+
+# Device and CPE Enums
 class DeviceType(str, Enum):
     ROUTER = "router"
     SWITCH = "switch"
     OLT = "olt"
 
+class CpeType(str, Enum):
+    WIFI_ROUTER = "wifi_router"
+    ONU = "onu"
+    ONT = "ont"
+
+# Configuration Enums
 class SnmpVersion(int, Enum):
     V1 = 1
     V2 = 2
     V3 = 3
+
+class GponOperState(str, Enum):
+    InitialState = 1
+    StandByState = 2
+    SerialNumberState = 3
+    RangingState = 4
+    OperationState = 5
+    PopupState = 6
+    EmergencyStopState = 7
