1ab2523ecba8f3b54605e9ff253efad7c8483b2b
Felipe Duellis Padilha <felipe.padilha@brasilnetcp.net.br>
2025-04-11 09:35:36 -0300

    feat: added CLI command suport to db operations

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -2,5 +2,7 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-# Importa os models para que o Alembic reconheça
-from .user import User
+import app.models.user
+import app.models.device
+import app.models.cpe
+import app.models.customer
